b3c2a25a3bbeb7efcba1bfbecfb4c32b09f85bbd
@gandalfg <36696601+adaOctopus@users.noreply.github.com>
2025-02-19 16:16:58 +0100

    Update linkedin.py

--- linkedin.py
+++ linkedin.py
@@ -11,6 +11,8 @@ options = Options()
 options.add_argument("start-maximized")
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://www.linkedin.com/in/<YOURPROFILENAMEASAPPEARSINURL>/recent-activity/all/")
+commentsUrl = "https://www.linkedin.com/in/<YOURPROFILENAMEASAPPEARSINURL>/recent-activity/comments/"
+#driver.get()
 wait = WebDriverWait(driver, 10)
 url = "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin/"
 driver.get(url)
@@ -23,7 +25,22 @@ wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class,'glob
 #wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='logout']"))).click()
 time.sleep(1)
 urltwo = "https://www.linkedin.com"
-driver.get(urltwo)
+driver.get(postUrl)
+y = 1000
+for i in range(0,10):
+    driver.execute_script("window.scrollTo(0, "+str(y)+")")
+    y += 1000  
+    time.sleep(1)
+
+page_source = driver.page_source
+print('your are here now')
+fileToWrite = open(r"<yourpath>\linkedin.html", "w+", encoding="utf-8")
+print('writing to file')
+fileToWrite.write(page_source)
+fileToWrite.close()
+
+while(True):
+    pass
 
 for i in range(0,10):
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
