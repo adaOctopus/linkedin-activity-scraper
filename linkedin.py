ef344b665ebe10fd5b0dbfded1f20d506e80c10e
@gandalfg <36696601+adaOctopus@users.noreply.github.com>
2025-02-20 17:16:38 +0100

    Going through csv with pandas to scrape posts

--- linkedin.py
+++ linkedin.py
@@ -24,20 +24,43 @@ wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-litms-control-urn
 wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class,'global-nav__primary-link')][contains(.,'Me')]"))).click()
 #wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='logout']"))).click()
 time.sleep(1)
-urltwo = "https://www.linkedin.com"
-driver.get(postUrl)
-y = 1000
-for i in range(0,10):
-    driver.execute_script("window.scrollTo(0, "+str(y)+")")
-    y += 1000  
-    time.sleep(1)
+#driver.quit()
+y=1000
 
-page_source = driver.page_source
-print('your are here now')
-fileToWrite = open(r"<yourpath>\linkedin.html", "w+", encoding="utf-8")
-print('writing to file')
-fileToWrite.write(page_source)
-fileToWrite.close()
+df = pd.read_csv(r"yourpathtofile\testFile.csv", usecols = ['First Name','URL', 'Position'])
+#, usecols = ['First Name','URL', 'Position']
+print(df)
+userName = df['First Name']
+userURL = df['URL']
+userPosition = df['Position']
+print(type(userName))
+k = 0
+# if you want to iterate through both columns at the same time and same index
+# EXAMPLE
+#stu_df = pd.DataFrame(students, columns=['Name', 'Age', 'Section'],
+#                      index=['1', '2', '3', '4'])
+ 
+# gives a tuple of column name and series
+# for each column in the dataframe
+#for (columnName, columnData) in stu_df.iteritems():
+#    print('Column Name : ', columnName)
+#    print('Column Contents : ', columnData.values)
+for x in df['URL']:
+    driver.get(x+"/recent-activity/all/")
+    k += 1
+    for i in range(0,15):
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, "+str(y)+")")
+        y += 1000  
+        time.sleep(1)
+        print(i)
+        ## this needs to be one index lower than the top level of the range
+        if i == 14:
+            page_source = driver.page_source
+            print('here')
+            fileToWrite = open(fr'C:\Users\tasos\Downloads\linkedin\linkedinPosts{str(k)}.html',"w+",encoding="utf-8")
+            fileToWrite.write(page_source)
+            fileToWrite.close()
 
 while(True):
     pass
